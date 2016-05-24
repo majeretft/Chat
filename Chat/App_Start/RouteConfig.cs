73c31810e5c9455083423e96f40156a17788549b
majeretft <majeretft@mail.ru>
2016-05-24 09:01:12 +0300

    Starting template

---
+++ Chat/App_Start/RouteConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Chat
+{
+	public class RouteConfig
+	{
+		public static void Register(RouteCollection routes)
+		{
+			routes.MapRoute(null, "{controller}/{action}", new { controller = "User", action = "LogIn" });
+		}
+	}
+}
---
+++ Chat/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Chat.Controllers
+{
+	public class UserController : Controller
+	{
+		public ViewResult LogIn()
+		{
+			return View();
+		}
+	}
+}
