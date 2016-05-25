55666afcd16f1d52dc495b87edeabdb60861a82c
majeretft <majeretft@mail.ru>
2016-05-25 16:31:29 +0300

    Added forms switching with buttons

--- Chat/Scripts/chat.js
+++ Chat/Scripts/chat.js
@@ -1,13 +1,18 @@
-﻿var Chat = function() {
+﻿var Chat = function () {
 	this.model = {
 		login: {
 			login: ko.observable(),
-			password: ko.observable()
+			password: ko.observable(),
+			isActiveView: ko.observable(true)
 		},
 		register: {
 			name: ko.observable(),
 			email: ko.observable(),
-			password: ko.observable()
+			password: ko.observable(),
+			isActiveView: ko.observable(false)
+		},
+		chatWindow: {
+			isActiveView: ko.observable(false)
 		}
 	};
 };
@@ -15,16 +20,19 @@
 Chat.prototype = {
 	constructor: Chat,
 
-	onLoginClick: function() {
-		
+	onLoginClick: function(model) {
+		model.login.isActiveView(false);
+		model.chatWindow.isActiveView(true);
 	},
 
-	onGotoRegisterClick: function() {
-		
+	onGotoRegisterClick: function (model) {
+		model.login.isActiveView(false);
+		model.register.isActiveView(true);
 	},
 
-	onRegisterClick: function() {
-		
+	onRegisterClick: function (model) {
+		model.register.isActiveView(false);
+		model.login.isActiveView(true);
 	}
 };
 
