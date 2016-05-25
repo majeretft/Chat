e218614982d3d34b81ebbe99c895a67bfd51da7a
majeretft <majeretft@mail.ru>
2016-05-25 11:48:45 +0300

    Minor changes

--- Chat/Models/RegisterModel.cs
+++ Chat/Models/RegisterModel.cs
@@ -2,10 +2,13 @@
 
 namespace Chat.Models
 {
-	public class LogInModel
+	public class RegisterModel
 	{
 		[Required]
-		public string Login { get; set; }
+		public string Name { get; set; }
+
+		[Required]
+		public string Email { get; set; }
 
 		[Required]
 		public string Password { get; set; }
