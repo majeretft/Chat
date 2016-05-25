2e1da2a7d16335bea3098f465d4a0d4bab9e4330
majeretft <majeretft@mail.ru>
2016-05-25 11:28:40 +0300

    Added bundles

--- Chat/App_Start/BundleConfig.cs
+++ Chat/App_Start/BundleConfig.cs
@@ -8,6 +8,11 @@ namespace Chat
 		{
 			bundles.Add(new StyleBundle("~/css/bootstrap.css").Include("~/Content/bootstrap.css"));
 			bundles.Add(new StyleBundle("~/css/chat.css").Include("~/Content/chat.css"));
+
+			bundles.Add(new ScriptBundle("~/js/jquery.js").Include("~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/js/bootstrap.js").Include("~/Scripts/bootstrap.js"));
+			bundles.Add(new ScriptBundle("~/js/knockout.js").Include("~/Scripts/knockout-{version}.js"));
+			bundles.Add(new ScriptBundle("~/js/chat.js").Include("~/Scripts/chat.js"));
 		}
 	}
 }
